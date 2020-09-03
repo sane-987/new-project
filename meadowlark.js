c7ea64f09b44c0ebde51b164d2ce47cf43e7f83f
sane-987 <shriyashsutar987@gmail.com>
2020-09-03 20:50:45 +0530

    second commit

--- meadowlark.js
+++ meadowlark.js
@@ -1,10 +1,11 @@
+var fortune = require('./lib/fortune');
 var express = require('express');
 var app = express();
 
 var handlebars = require('express3-handlebars')
                 .create({defaultLayout:'main'});
 
-var fortunes = ['ocalnclk','qwe','erty',];
+
 
 app.engine('handlebars',handlebars.engine);
 app.set('view engine', 'handlebars');
@@ -17,8 +18,7 @@ app.get('/', (req,res) => {
 });
 
 app.get('/about', (req,res) => {
-    var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-    res.render('about',{fortune:randomFortune});
+    res.render('about',{fortune: fortune.getFortune() });
 });
 
 
